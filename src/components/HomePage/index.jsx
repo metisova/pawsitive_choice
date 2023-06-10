56b21e473aa2a6bd6b4e3d2438e2999155062085
Anna Kopylova <anna.metisova@gmail.com>
2023-06-10 14:06:11 +0200

    created HomePage component

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+import { RunningLine } from '../RunningLine';
+import rabbitImage from '../../img/rabbit_new.png';
+
+export const HomePage = ({setDarkMode}) => {
+  const handleRabbitClick = () => {
+    setTimeout(() => {
+      setDarkMode(true);
+    }, 1000);
+  };
+
+  return (
+    <>
+  <main>
+    <div className="running-line-container">
+      <RunningLine speed={1} />
+      <RunningLine speed={3} />
+      <RunningLine speed={5} />
+      <RunningLine speed={7} />
+      <div className="intro-img">
+        <img
+          className="rabbit-img"
+          src={rabbitImage}
+          alt="Rabbit"
+          onClick={handleRabbitClick}
+        />
+      </div>
+    </div>
+  </main>;
+  </>
+  )
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
-import { RunningLine } from './components/RunningLine';
 import { Header } from './components/Header';
-import rabbitImage from './img/rabbit_new.png';
-import cageImage from './img/cage.png';
+import { HomePage } from './components/HomePage';
 
 const App = () => {
-  const [cage, setCage] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
-
-  const handleRabbitClick = () => {
-    setCage(true);
-    setTimeout(() => {
-      setDarkMode(true);
-    }, 1000);
-  };
-
-  const handleCageClick = () => {
-    setCage(false);
-    setDarkMode(false);
-  };
 
   const switchTheme = () => {
     if (darkMode) {
@@ -37,32 +22,7 @@ const App = () => {
   return (
     <>
       <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
-      <main>
-        <div className="running-line-container">
-          <RunningLine speed={1} />
-          <RunningLine speed={3} />
-          <RunningLine speed={5} />
-          <RunningLine speed={7} />
-          <div className="intro-img">
-            <img
-              className="rabbit-img"
-              src={rabbitImage}
-              alt="Rabbit"
-              onClick={handleRabbitClick}
-            />
-          </div>
-          {cage && (
-            <div className="intro-img-top">
-              <img
-                className="cage-img"
-                src={cageImage}
-                alt="Cage"
-                onClick={handleCageClick}
-              />
-            </div>
-          )}
-        </div>
-      </main>
+      <HomePage setDarkMode={setDarkMode} />
     </>
   );
 };
