4aaa0d08921cff80c41ef30943c51707efa01a7d
Anna Kopylova <anna.metisova@gmail.com>
2023-06-11 11:30:36 +0200

    fixed running text

--- src/components/RunningLine/index.jsx
+++ src/components/RunningLine/index.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './style.css';
 
 export const RunningLine = ({ speed }) => {
+  const runningText =
+    'this project aims to increase awareness about animal testing this project aims to increase awareness about animal testing&nbsp;';
+
   return (
     <p className="running-line">
-      <span className={`running-line-text speed${speed}`}>
-        this project aims to increase awareness about animal testing 3 this
-        project aims to increase awareness about animal testing&nbsp;
-      </span>
+      <span className={`running-line-text speed${speed}`}>{runningText}</span>
       <span className={`running-line-text speed${speed + 1}`}>
-        this project aims to increase awareness about animal testing 3 this
-        project aims to increase awareness about animal testing&nbsp;
+        {runningText}
       </span>
     </p>
   );
