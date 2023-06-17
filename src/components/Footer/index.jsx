1a3b620e88b87fd6d56503df32b6c644faf90905
Anna Kopylova <anna.metisova@gmail.com>
2023-06-17 20:00:44 +0200

    fixed footer position for mobile version

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import heart from '../../img/heart_icon.png';
 export const Footer = () => (
   <footer>
     <div className="footer-container">
-      <p className="footer-text">Made with <img src={heart} className="icon-heart" /> by Anna Kopylova © 2023 </p>
+      <p className="footer-text">Made with <img src={heart} alt="heart emoji" className="icon-heart" /> by Anna Kopylova © 2023 </p>
     </div>
   </footer>
 );
