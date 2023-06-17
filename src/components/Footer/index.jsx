7877be28a808c741cd1863352e7d13448955a9d8
Anna Kopylova <anna.metisova@gmail.com>
2023-06-17 17:54:50 +0200

    added page footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.css';
+import heart from '../../img/heart_icon.png';
 
 export const Footer = () => (
   <footer>
-    <div className="container">
-      
+    <div className="footer-container">
+      <p class="footer-text">Made with <img src={heart} class="icon-heart" /> by Anna Kopylova © 2023 </p>
     </div>
   </footer>
 );
